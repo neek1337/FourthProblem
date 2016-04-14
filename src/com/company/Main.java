5d091a5738b61a44540643b8c3a60ef7e2ef6fc5
neek1337 <vdo13@list.ru>
2016-04-14 08:07:51 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,51 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-       // new Frequency().run();
         Scanner scanner = new Scanner(System.in);
-        new Decrypt().run(scanner);
-       // new Encrypt().run();
+        System.out.println("Выберите действие:");
+        System.out.println("1 - Вычисление частотных таблиц языка;");
+        System.out.println("2 - Шифрование текста");
+        System.out.println("3 - Расшифрование языка");
+        System.out.println("4 - Перебор возможных ключей;");
+        System.out.println("5 - Шифрование текста.");
+
+        int n = scanner.nextInt();
+
+        switch (n) {
+            case (1): {
+                new Frequency().run();
+                break;
+            }
+
+            case (2): {
+                new Encrypt().run();
+                break;
+            }
+
+            case (3): {
+                new Decrypt().run(scanner);
+                break;
+            }
+
+            case (4): {
+
+                break;
+            }
+
+            case (5): {
+
+                break;
+            }
+
+            default: {
+                System.out.println("Введеное число должно быть в диапозоне от 1 до 5.");
+            }
+            //
+
+
+            // new Encrypt().run();
+        }
+
+
     }
-
-
 }
